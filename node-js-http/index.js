c6089ffb3eff6ca75e647c794453b80e08a50701
tsubasa1122 <nakanot@open8.com>
2020-09-12 21:17:21 +0900

    Add console request address

--- node-js-http/index.js
+++ node-js-http/index.js
@@ -2,6 +2,9 @@
 
 const http = require('http');
 const server = http.createServer((req, res) => {
+  console.info(
+    '[' + new Date() + '] Request by ' + req.connection.remoteAddress
+  )
   res.writeHead(200, {
     'Content-Type': 'text/html; charset=utf-8'
   });
