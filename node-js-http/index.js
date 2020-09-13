1829e306351cec3abb712733659e5cfeea748ccf
tsubasa1122 <nakanot@open8.com>
2020-09-14 00:32:36 +0900

    Add pug template

--- node-js-http/index.js
+++ node-js-http/index.js
@@ -1,47 +1,63 @@
 'use strict';
-
 const http = require('http');
-const server = http.createServer((req, res) => {
-  console.info(
-    '[' + new Date() + '] Request by ' + req.connection.remoteAddress
-  )
-  res.writeHead(200, {
-    'Content-Type': 'text/html; charset=utf-8'
-  });
-  switch(req.method) {
-    case 'GET':
-        const fs = require('fs');
-        const rs = fs.createReadStream('./form.html');
-        rs.pipe(res);
-      break;
-    case 'POST':
-      res.write('POST' + req.url);
-      let rawData = '';
-      req.on('data', chunk => {
-        rawData = rawData + chunk;
-      }).on('end', () => {
-        const decoded = decodeURIComponent(rawData);
-        console.info('[' + new Date() + '] 投稿: ' + decoded);
-        res.write('<!DOCTYPE html><html lang="ja"><body><h1>' + decoded + 'が投稿されました</h1></body></html>');
-        res.end()
-      });
-      break;
-    case 'DELETE':
-      res.write('DELETE ' + req.url);
-      res.end();
-      break;
-    default:
-      break;
-  }
-})
-.on('error', e => {
-  console.error('[' + new Date() + '[ Server Error', e);
-})
-.on('clientError', e => {
-  console.error('[' + new Date() + '] Client Error', e);
-});
+const pug = require('pug');
+const server = http
+  .createServer((req, res) => {
+    const now = new Date();
+    console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    });
 
+    switch (req.method) {
+      case 'GET':
+        if (req.url === '/enquetes/yaki-shabu') {
+          res.write(
+            pug.renderFile('./form.pug', {
+              path: req.url,
+              firstItem: '焼き肉',
+              secondItem: 'しゃぶしゃぶ'
+            })
+          );
+        } else if (req.url === '/enquetes/rice-bread') {
+          res.write(
+            pug.renderFile('./form.pug', {
+              path: req.url,
+              firstItem: 'ごはん',
+              secondItem: 'パン'
+            })
+          );
+        }
+        res.end();
+        break;
+      case 'POST':
+        let rawData = '';
+        req
+          .on('data', chunk => {
+            rawData = rawData + chunk;
+          })
+          .on('end', () => {
+            const qs = require('querystring');
+            const answer = qs.parse(rawData);
+            const body = answer['name'] + 'さんは' +
+              answer['favorite'] + 'に投票しました';
+            console.info('[' + now + '] ' + body);
+            res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
+              body + '</h1></body></html>');
+            res.end();
+          });
+        break;
+      default:
+        break;
+    }
+  })
+  .on('error', e => {
+    console.error('[' + new Date() + '] Server Error', e);
+  })
+  .on('clientError', e => {
+    console.error('[' + new Date() + '] Client Error', e);
+  });
 const port = 8000;
 server.listen(port, () => {
-  console.log('Listening on ' + port);
+  console.info('[' + new Date() + '] Listening on ' + port);
 });
